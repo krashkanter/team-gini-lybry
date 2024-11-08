763b6fafb11aefba780d1acf0ad26ae522fc2311
Aryan <justaryansingh05@gmail.com>
2024-11-08 20:42:25 +0530

    first

--- lib/screens/Library.dart
+++ lib/screens/Library.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 import 'package:sahyadri_hacknight/reusable.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,6 +12,36 @@ class Library extends StatefulWidget {
 }
 
 class _LibraryState extends State<Library> {
+  List<Map<String, dynamic>> books = []; // Variable to store fetched data
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBooks(); // Fetch books when the widget is initialized
+  }
+
+  Future<void> fetchBooks() async {
+    try {
+      // Reference to Firestore
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      // Fetching the collection
+      QuerySnapshot querySnapshot = await firestore.collection('books').get();
+
+      // Extracting data from the documents
+      List<Map<String, dynamic>> fetchedBooks = querySnapshot.docs.map((doc) {
+        return {'id': doc.id, ...doc.data() as Map<String, dynamic>};
+      }).toList();
+
+      // Update the state with the fetched data
+      setState(() {
+        books = fetchedBooks;
+      });
+    } catch (e) {
+      print("Error getting documents: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,11 +64,30 @@ class _LibraryState extends State<Library> {
                       ),
                     )),
               )),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Divider(
+              color: textC,
+              thickness: 6,
+            ),
+          ),
           Align(
             alignment: Alignment(0, 1),
             child: Container(
-              color: textC,
-              height: MediaQuery.sizeOf(context).height - 120,
+              color: Colors.transparent,
+              height: MediaQuery.sizeOf(context).height - 130,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: books.map((book) {
+                    // Use null-aware operators to handle potential null values
+                    final link = book['link'] ??
+                        'https://www.shutterstock.com/shutterstock/photos/2275148213/display_1500/stock-vector-naive-playful-abstract-shapes-sticker-pack-groovy-circle-oval-rectangle-arch-eyes-typography-in-2275148213.jpg'; // Default to empty string if null
+                    final title = book['title'] ??
+                        'Untitled'; // Default to 'Untitled' if null
+                    return Book(context, link, title, book['text']);
+                  }).toList(),
+                ),
+              ),
             ),
           )
         ],
