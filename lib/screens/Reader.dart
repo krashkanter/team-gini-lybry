8ae91bd098450ad70d672363dc504163c4778ed5
Aryan <justaryansingh05@gmail.com>
2024-11-08 22:07:49 +0530

    first

--- lib/screens/Reader.dart
+++ lib/screens/Reader.dart
@@ -14,8 +14,39 @@ class Reader extends StatefulWidget {
 }
 
 class _ReaderState extends State<Reader> {
+  late List<String> words;
+  int currentIndex = 0;
+  static const int wordsPerPage = 20; // Number of words to display at a time
+
+  @override
+  void initState() {
+    super.initState();
+    // Split the string into words
+    words = widget.data.split(' ');
+  }
+
+  void nextPage() {
+    setState(() {
+      if (currentIndex + wordsPerPage < words.length) {
+        currentIndex += wordsPerPage;
+      }
+    });
+  }
+
+  void previousPage() {
+    setState(() {
+      if (currentIndex - wordsPerPage >= 0) {
+        currentIndex -= wordsPerPage;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Get the words to display based on the current index
+    List<String> displayedWords =
+        words.skip(currentIndex).take(wordsPerPage).toList();
+
     return Scaffold(
       backgroundColor: bgC,
       appBar: AppBar(
@@ -23,45 +54,102 @@ class _ReaderState extends State<Reader> {
         toolbarHeight: 100,
         backgroundColor: bgC,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-              child: StoryContainer(str: widget.data.substring(0, 100)),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: StoryContainer(
+                  words: displayedWords,
+                  onWordTap: (word) {
+                    print(word);
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: currentIndex > 0 ? previousPage : null,
+                      child: Text('Previous'),
+                    ),
+                    ElevatedButton(
+                      onPressed: currentIndex + wordsPerPage < words.length
+                          ? nextPage
+                          : null,
+                      child: Text('Next'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Align(
+            alignment: Alignment(-1, 1),
+            child: GestureDetector(
+              onTap: currentIndex + wordsPerPage < words.length
+                  ? previousPage
+                  : null,
               child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 100,
                 decoration: BoxDecoration(
-                  border: Border.all(color: toColor("333A3F"), width: 5),
-                  color: bgC,
-                  borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: toColor("333A3F"), width: 5),
+                    color: toColor("56C0A1"),
+                    borderRadius: BorderRadius.circular(1000)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    size: 40,
+                  ),
                 ),
               ),
             ),
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Align(
+            alignment: Alignment(1, 1),
+            child: GestureDetector(
+              onTap:
+                  currentIndex + wordsPerPage < words.length ? nextPage : null,
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: toColor("333A3F"), width: 5),
+                    color: toColor("56C0A1"),
+                    borderRadius: BorderRadius.circular(1000)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.arrow_forward_rounded,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
 
 class StoryContainer extends StatelessWidget {
-  final String str;
+  final List<String> words;
+  final ValueChanged<String> onWordTap;
 
-  StoryContainer({required this.str});
+  StoryContainer({required this.words, required this.onWordTap});
 
   @override
   Widget build(BuildContext context) {
-    // Split the string into words
-    List<String> words = str.split(' ');
-
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: 400,
       decoration: BoxDecoration(
         border: Border.all(color: Color(0xFF333A3F), width: 5),
         color: bgC, // Replace with your bgC variable
@@ -73,27 +161,25 @@ class StoryContainer extends StatelessWidget {
           spacing: 8.0, // Space between words
           children: words.map((word) {
             return GestureDetector(
-                onTap: () {
-                  // Print the word to the console when tapped
-                  print(word);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: toColor("000000")
-                          .withAlpha(30), // Replace with your bgC variable
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
-                      child: Text(
-                        word,
-                        style: TextStyle(fontSize: 28),
-                      ),
+              onTap: () => onWordTap(word),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: toColor("000000")
+                        .withAlpha(30), // Replace with your bgC variable
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                    child: Text(
+                      word,
+                      style: TextStyle(fontSize: 28),
                     ),
                   ),
-                ));
+                ),
+              ),
+            );
           }).toList(),
         ),
       ),
