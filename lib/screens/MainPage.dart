9215270959982c833d29f57db5b7a6fabc1cee46
Aryan <justaryansingh05@gmail.com>
2024-11-08 17:22:58 +0530

    first

--- lib/screens/MainPage.dart
+++ lib/screens/MainPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sahyadri_hacknight/reusable.dart';
+import 'package:sahyadri_hacknight/screens/Library.dart';
 
 class Mainpage extends StatefulWidget {
   const Mainpage({super.key});
@@ -17,8 +18,31 @@ class _MainpageState extends State<Mainpage> {
       body: Stack(
         children: [
           Align(
-              alignment: Alignment(0, -0.5),
+              alignment: const Alignment(0, -0.5),
               child: logoWidget("assets/LyBry-Transparent.png", 300, 300)),
+          const Align(
+              alignment: Alignment(0, -0.05),
+              child: Text(
+                "LyBry",
+              )),
+          Align(
+            alignment: Alignment(0, 1),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => const Library()));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: toColor("56C0A1"),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(200),
+                        topRight: Radius.circular(200)), // Rounded corners
+                  ),
+                  width: 300,
+                  height: 300,
+                )),
+          )
         ],
       ),
     );
